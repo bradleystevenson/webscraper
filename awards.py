7b920c29036267c42b9aab5d443c0b96e6819409
Bradley Stevenson <bradley.stevenson@grabcad.com>
2023-09-13 10:41:01 -0400

    Checkpoint commit, continuing to work on awards

--- awards.py
+++ awards.py
@@ -1,9 +1,9 @@
 from object import Object
-from table import Table, NoMatchException
+from table import Table, ObjectTable, NoMatchException
 from webscraper import fetch_soup_from_page
 from table_parser import TableParser
 from data_dict_from_object import DataDictFromObject
-from common_parser_functions import row_has_link, get_element_with_attributes, get_text_of_element_with_attributes, is_header_numeric
+from common_parser_functions import row_has_link, get_element_with_attributes, get_text_of_element_with_attributes, is_header_numeric, get_url_of_element_with_attributes
 
 class Awards(Object):
 
@@ -16,19 +16,70 @@ class Awards(Object):
         self.hall_of_fame_coaches_table = Table('hall_of_fame_coaches', ['coach_id', 'year_inducted'])
         self.hall_of_fame_contributors_table = Table('hall_of_fame_contributors', ['contributor_name', 'year_inducted', 'start_year', 'last_year', 'induction_role', 'roles'])
         self.hall_of_fame_ballots_table = Table('hall_of_fame_ballots', ['year', 'ballot', 'role', 'name', 'coach_id', 'player_id', 'executive_id', 'inducted'])
-        super().__init__(create_from_web, [self.hall_of_fame_players_table, self.hall_of_fame_coaches_table, self.hall_of_fame_contributors_table])
+        self.top_100_players_table = Table('top_100_players', ['year', 'player_id'])
+        self.all_pro_awards_table = ObjectTable('all_pro_awards', ['all_pro_award_id', 'award_name'], 'all_pro_award_id')
+        super().__init__(create_from_web, [self.hall_of_fame_players_table, self.hall_of_fame_coaches_table, self.hall_of_fame_contributors_table,
+                                           self.hall_of_fame_ballots_table, self.top_100_players_table])
 
     def _create_from_web(self):
         #self._create_hall_of_fame()
         links = self._create_links()
-        self._create_hall_of_fame_ballots(links['hall_of_fame_ballots'])
+        #self._create_top_100(links['top_100_players'])
+        #self._create_hall_of_fame_ballots(links['hall_of_fame_ballots'])
+        self._create_nfl_all_pros([{'url': '/years/2022/allpro.htm'}])
+
+
+    def _create_nfl_all_pros(self, links):
+        for link in links:
+            table_parser = TableParser(fetch_soup_from_page(f'https://www.pro-football-reference.com{link["url"]}').find(id="all_pro"),
+                                       row_has_link, DataDictFromObject({'position': get_text_of_element_with_attributes({'data-stat': 'pos'}),
+                                                                         'player_url': get_url_of_element_with_attributes({'data-stat': 'player'}),
+                                                                         'all_pros': get_text_of_element_with_attributes({'data-stat': 'all_pro_string'})}))
+            for data_dict in table_parser.data:
+                for all_pro in data_dict['all_pros'].split(', '):
+                    try:
+                        self.all_pro_awards_table.get_primary_key_by_columns_search({'award_name': all_pro})
+                    except NoMatchException:
+                        self.all_pro_awards_table.append({'award_name': all_pro})
+
+
+
+    def _create_top_100(self, links):
+        for link in links:
+            table_parser = TableParser(fetch_soup_from_page('https://www.pro-football-reference.com' + link['url']).find(id="top_100"),
+                                       is_header_numeric,
+                                       DataDictFromObject({'player_url': get_url_of_element_with_attributes({'data-stat': 'player'})}, hardcoded_dict={'year': link['year']}))
+            for data_dict in table_parser.data:
+                print(data_dict)
+                data_dict['player_id'] = self.players.players_table.get_primary_key_by_columns_search({'player_url': data_dict['player_url']})
+                self.top_100_players_table.append(data_dict)
 
     def _create_links(self):
+        return_dict = {'hall_of_fame_ballots': [], 'top_100_players': [], 'all_pros': [], 'other_all_pros': [],
+                       'voting_summaries': [], 'all_decade_teams': [], 'all_rookie_teams': [], 'defunct_awards': [], 'awards': []}
         soup = fetch_soup_from_page("https://www.pro-football-reference.com/awards/")
-        hall_of_fame_ballot_links = []
         for p in soup.find(text="Pro Football Hall of Fame Balloting").parent.parent.find_all("p"):
-            hall_of_fame_ballot_links.append({'year': p.text, 'url': p.find("a")['href']})
-        return {'hall_of_fame_ballots': hall_of_fame_ballot_links}
+            return_dict['hall_of_fame_ballots'].append({'year': p.text, 'url': p.find("a")['href']})
+        for p in soup.find(text="NFL Top 100 Players").parent.parent.find_all("p"):
+            return_dict['top_100_players'].append({'url': p.find("a")['href'], 'year': p.text.split(' ')[0]})
+        for p in soup.find(text="NFL All-Pros").parent.parent.find_all("p"):
+            return_dict['all_pros'].append({'url': p.find("a")['href'], 'year': p.text})
+        for p in soup.find(text="Other All-Pros").parent.parent.find_all("p"):
+            return_dict['other_all_pros'].append({'year': p.text.split(' ')[0], 'league': p.text.split(' ')[1], 'url': p.find("a")['href']})
+        for p in soup.find(text='Football Award Voting Summaries').parent.parent.find_all('p'):
+            return_dict['voting_summaries'].append({'year': p.text, 'url': p.find('a')['href']})
+        for p in soup.find(text="All-Decade Teams").parent.parent.find_all('p'):
+            return_dict['all_decade_teams'].append({'year': p.text.split('-')[1], 'url': p.find('a')['href'],
+                                                    'award': p.text.split(' All')[0]})
+        for p in soup.find(text="NFL All-Rookie Teams").parent.parent.find_all('p'):
+            return_dict['all_rookie_teams'].append({'year': p.text.split(' ')[0], 'url': p.find('a')['href']})
+        for p in soup.find(text="Defunct Awards").parent.parent.find_all('p'):
+            return_dict['defunct_awards'].append({'url': p.find('a')['href'], 'award_name': p.find('a').text, 'leagues': p.find('span').text.split(', ')[0].split(',')})
+        for p in soup.find(text="Pro Football Awards").parent.parent.find_all("p"):
+            return_dict['awards'].append({'url': p.find('a')['href'], 'award_name': p.text})
+        print(return_dict)
+        return return_dict
+
 
 
     def _create_hall_of_fame_ballots(self, links):
--- data_dict_from_object.py
+++ data_dict_from_object.py
@@ -1,11 +1,15 @@
 class DataDictFromObject:
 
 
-    def __init__(self, fields_function_dict):
+    def __init__(self, fields_function_dict, hardcoded_dict=None):
         self.fields_function_dict = fields_function_dict
+        self.hardcoded_dict = hardcoded_dict
 
     def parse(self, object):
         data = {}
         for field in self.fields_function_dict.keys():
             data[field] = self.fields_function_dict[field](object)
+        if self.hardcoded_dict is not None:
+            for field in self.hardcoded_dict.keys():
+                data[field] = self.hardcoded_dict[field]
         return data
