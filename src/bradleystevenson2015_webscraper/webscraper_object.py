b8047ab9059f0f0cb7c9ecde34adb3b3594b9332
Bradley Stevenson <bradleystevenson2015@gmail.com>
2023-10-14 12:01:46 -0400

    Finished ticket

--- src/bradleystevenson2015_webscraper/webscraper_object.py
+++ src/bradleystevenson2015_webscraper/webscraper_object.py
@@ -81,13 +81,13 @@ class WebscraperObject:
     
 class NewWebscraperObject(WebscraperObject):
 
-    def __init__(self, object_name, table_name, parsers, url_generator, create_from_page_parser):
+    def __init__(self, object_name, parsers, url_generator, create_from_page_parser):
         self.object_name = object_name
         self.table_name = table_name
         self.parsers = parsers
         self.create_from_page_parser = create_from_page_parser
         self.url_generator = url_generator
-        super().__init__(object_name, [table_name], create_from_page_parser)
+        super().__init__(object_name, [object_name], create_from_page_parser)
 
     def create_from_web(self, webscraperObjectCollection):
         for url in self.url_generator.generate_urls(webscraperObjectCollection):
@@ -109,7 +109,7 @@ class WebscraperObjectFactory:
             for parser_dict in webscraper_object_dict['parsers']:
                 parsers.append(ParserObjectFactory(parser_dict).parser)
             url_generator = URLGeneratorFactory(webscraper_object_dict['urls'])
-            self.webscraper = NewWebscraperObject(webscraper_object_dict['object_name'], webscraper_object_dict['tables'][0], parsers, url_generator.get_url_generator(), self.create_from_page_parser)
+            self.webscraper = NewWebscraperObject(webscraper_object_dict['object_name'], parsers, url_generator.get_url_generator(), self.create_from_page_parser)
         elif webscraper_object_dict['object_type'] == 'custom_object':
             for custom_object in custom_objects:
                 if custom_object.object_name == webscraper_object_dict['object_name']:
